9fc20012106bb35e8ff38656fcb1f157781dab35
loadoff-kashiwagi-rena <kashiwagi@loadoff.jp>
2026-06-20 01:22:18 +0900

    ステップ5

--- lambda-express-api/index.js
+++ lambda-express-api/index.js
@@ -1,27 +1,42 @@
 // const serverlessExpress = require('@vendia/serverless-express')
 const express = require('express')
+const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager')
+const { fromIni } = require('@aws-sdk/credential-providers')
 const app = express();
 const serverless = require('serverless-http')
 const mysql = require('mysql2/promise')
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    password: 'password',
-    database: 'handson',
-    user: 'root',
+
+const client = new SecretsManagerClient({
+    region: 'ap-northeast-1',
+    credentials: fromIni({ profile: 'mvtk-refactoring' })
 })
+
+let pool
+
+async function getSecret() {
+    const command = new GetSecretValueCommand({ SecretId: 'handson/db'})
+    const response = await client.send(command)
+    return JSON.parse(response.SecretString)
+}
+
+async function getPool() {
+    if (pool) return pool
+    const secret = await getSecret()
+    pool = mysql.createPool(secret)
+    return pool
+}
 
 app.use(express.json());
 
 app.get('/health', (req, res) => res.send({"status":"ok"}));
 
 app.get('/users', async (req, res) => {
-    const [ rows ] = await pool.query('SELECT id, name FROM users')
+    const [ rows ] = await (await getPool()).query('SELECT id, name FROM users')
     res.send(rows)
 });
 
 app.get('/users/:id',  async (req, res) => {
-    const [ rows ] = await pool.query('SELECT id, name FROM users WHERE id = ?', [req.params.id])
+    const [ rows ] = await (await getPool()).query('SELECT id, name FROM users WHERE id = ?', [req.params.id])
     if (rows[0]) {
         res.send(rows[0])
     } else {
@@ -30,7 +45,7 @@ app.get('/users/:id',  async (req, res) => {
 });
 
 app.post('/users', async (req, res) => {
-    const [ result ] = await pool.query('INSERT INTO users (name) VALUES (?)', [req.body.name])
+    const [ result ] = await (await getPool()).query('INSERT INTO users (name) VALUES (?)', [req.body.name])
     res.status(201).send({ id: result.insertId, name: req.body.name })
 });
 
