7436a8f1f406f5ec9c2d14d3dc595f65c65ded0f
loadoff-kashiwagi-rena <kashiwagi@loadoff.jp>
2026-06-19 14:22:56 +0900

    express.jsのハンズオン終了

--- lambda-express-api/index.js
+++ lambda-express-api/index.js
@@ -1,9 +1,24 @@
 // const serverlessExpress = require('@vendia/serverless-express')
-const app = require('express')()
+const express = require('express')
+const app = express();
+const serverless = require('serverless-http')
+
+app.use(express.json());
 
 app.get('/health', (req, res) => res.send({"status":"ok"}));
 
-app.listen(3000, () => console.log('listening on http://localhost:3000'))
+app.get('/users', (req, res) => res.send([{ id: 1, name: "alice" }, { id: 2, name: "bob" }]));
+
+app.get('/users/:id', (req, res) => res.send({ id: req.params.id}));
+
+app.post('/users', (req, res) => res.send(req.body));
+
+
+if (require.main === module) {
+    app.listen(3000, () => console.log('listening on http://localhost:3000'))
+} else {
+    exports.handler = serverless(app);
+}
 
 
 // exports.handler = serverlessExpress({ app }) // これはLambda用
